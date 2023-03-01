242233448897d837ba9863e0921aedb82fb6e737
Julien1988 <julienbroens@gmail.com>
2023-03-01 16:23:04 +0100

    add js counter basket items

--- src/js/scrypt.js
+++ src/js/scrypt.js
@@ -1,1 +1,24 @@
-console.log('Hello Shop');
+// JS
+
+let addToBasket = document.getElementsByClassName("main__container__list__item__link");
+const getBasketCount = document.getElementById('itemCount');
+
+addToBasket = Array.from(addToBasket);
+
+let totalPrice = 0;
+
+addToBasket.forEach(e => e.addEventListener('click', function (e) {
+    e.preventDefault();
+    totalPrice += parseInt(e.target.dataset['price'])
+    if (totalPrice > 0) {
+        getBasketCount.classList.remove('hidden');
+        let str = getBasketCount.innerHTML
+        str = str.split(' ')
+        getBasketCount.innerHTML = totalPrice + ' ' + str[1];
+    }
+
+    console.log(totalPrice);
+}));
+
+
+
