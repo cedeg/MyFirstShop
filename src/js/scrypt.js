378a61fa3edabfcb8092b26a0ea29344866f75f4
Julien1988 <julienbroens@gmail.com>
2023-03-01 16:31:41 +0100

    Refacto ++

--- src/js/scrypt.js
+++ src/js/scrypt.js
@@ -1,13 +1,10 @@
 // JS
 
-let addToBasket = document.getElementsByClassName("main__container__list__item__link");
+const addToBasket = Array.from(document.getElementsByClassName("main__container__list__item__link"));
 const getBasketCount = document.getElementById('itemCount');
 
 let str = getBasketCount.innerHTML
 str = str.split(' ');
-
-
-addToBasket = Array.from(addToBasket);
 
 let totalPrice = parseInt(str[0]);
 
@@ -16,8 +13,8 @@ addToBasket.forEach(e => e.addEventListener('click', function (e) {
     totalPrice += parseInt(e.target.dataset['price'])
     if (totalPrice > 0) {
         getBasketCount.classList.remove('hidden');
-        let str = getBasketCount.innerHTML
-        str = str.split(' ')
+        let str = getBasketCount.innerHTML.split(' ')
+
         getBasketCount.innerHTML = totalPrice + ' ' + str[1];
     }
 
